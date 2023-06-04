881537097e3833373e8c870eb0df23f663e56f54
Andhromede <andhromede@hotmail.fr>
2023-06-04 18:24:52 +0200

    ajout tri cocktail

--- tri.py
+++ tri.py
@@ -58,19 +58,26 @@ for i in range(50):
 # ============================================================================
 # TRI A BULLES
 # ============================================================================ 
+# """
+# le tri a bulle compare 2 chiffres qui se suivent et les inversent si nécéssaire. 
+# Arrivé a la fin du tableau, il recommance depuis le début et ceux jusqu'au tri 
+# complet de tout le tableau.
 
-# permutation = True
-# passage = 0
+# switch = le flag qui permet de mettre fin a la boucle
+# nbTurn = permet d'exclure le(s) dernier(s) chiffre(s) du tableau (deja trié(s)) a chaque tour
+# """
 
-# while permutation == True:
-#     permutation = False
-#     passage += 1
+# switch = True
+# nbTurn = 0
 
-#     for i in range(0, len(tab) - passage):
-#         if tab[i] > tab[i + 1]:
-#             permutation = True
-#             tab[i], tab[i + 1] = \
-#             tab[i + 1],tab[i]
+# while switch == True:
+#     nbTurn += 1
+#     switch = False
+
+#     for i in range(0, len(tab) - nbTurn):
+#         if tab[i+1] < tab[i]:
+#             switch = True
+#             tab[i], tab[i + 1] = tab[i + 1], tab[i]
 
 # print(tab)
 
@@ -78,8 +85,42 @@ for i in range(50):
 # ============================================================================
 # TRI COCKTAIL 
 # ============================================================================ 
+# """
+# le tri cocktail fonctionne comme le tri à bulle sauf qu'il vas vers l'avant puis vers l'arrière
+# switch = le flag qui permet de stopper la boucle
+# sens = pour permettre au "curseur" d'aller en avant puis en arrière
+# i = "curseur"
+# debut = index de départ du tableau
+# fin = longueur du tableau -2 afin de ne pas essayer de comparer le dernier chiffre avec le suivant (qui n'éxiste pas)
+# """
 
 
+## permutation, sens, i = True, 1, 0
+# switch = True 
+# sens = 1
+# i = 0
+# debut = 0
+# fin = len(tab)-2
+
+# while switch == True:
+#     switch = False
+#     while (i < fin and sens == 1) or (i > debut and sens == -1) :
+#         # Si le chiffre actuelle est > au suivant ...
+#         if tab[i] > tab[i + 1]:
+#             switch = True
+#             # echange les deux elements
+#             tab[i], tab[i + 1] = tab[i + 1], tab[i]
+#         # fait avancer le "curseur" vers l'avant ou vers l'arrière
+#         i = i + sens
+    
+#     # On change le sens du parcours
+#     if sens == 1:
+#         fin = fin - 1
+#     else:
+#         debut = debut + 1
+#     sens = -sens
+
+# print(tab)
 
 # ============================================================================
 # TRI A PEIGNE 
